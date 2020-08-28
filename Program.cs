3c5d00bc3d6a6f75327c3bab8c95930ff0a50767
Gustav Rubæk <gullerubaek@gmail.com>
2020-08-29 01:19:43 +0200

    c# :)

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Rubaek21.github.io
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
